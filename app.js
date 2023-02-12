a0846bff4ec60a419f9430fb07b9b0240f7d4aff
Omed0 <omedakram54@gmail.com>
2023-02-13 00:42:25 +0300

    fix Create blog and delete methode

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
+const morgan = require('morgan') //for catch status and route
 const mongoose = require('mongoose')
 require('dotenv').config()
 const Blog = require('./models/blog')
 
 
+
 //connect to mongo db
-const dbURI = process.env.DBURI;
+const MongoDB = process.env.MONGO;
 mongoose.set('strictQuery', true)
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => app.listen(3000))
     .catch((err) => console.log(err))
 
@@ -20,43 +21,14 @@ app.set('view engine', 'ejs')
 //allow file in middleware to access
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+});
 //midlleware      {morgan}
 app.use(morgan('dev'))
 
-app.get('/add-blog', (req, res)=>{
-    const blog = new Blog({
-        title: 'new Blog 2',
-        snippet: 'about my new blog 2',
-        body: 'more about my new blog 2'
-    })
-    blog.save()
-        .then((result)=>{
-            res.send(result)
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-})
 
-app.get('/all-blogs', (req, res)=>{
-    Blog.find()
-        .then((result)=>{
-            res.send(result)
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-})
-
-app.get('/single-blog', (req, res)=>{
-    Blog.findById('63e6bd732f0b6697b5fb3d2c')
-        .then((result)=>{
-            res.send(result)
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-})
 
 //routes
 //home page
@@ -69,40 +41,50 @@ app.get('/about', (req, res) => {
     res.status(200).render('about', { title: 'about' })
 })
 
+
 //blog routes
-app.get('/blogs',(req, res)=>{
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog' })
+})
+
+app.get('/blogs', (req, res) => {
     Blog.find().sort({ createdAt: -1 /* reverse i henanaway datakan */ })
-        .then((result)=>{
-             res.status(200).render('index', { title: 'All Blogs', blogs: result })
+        .then((result) => {
+            res.status(200).render('index', { title: 'All Blogs', blogs: result })
         })
-        .catch((err)=>{
+        .catch((err) => {
             console.log(err);
         })
 })
 
-app.post('/blogs', (req, res)=>{
+app.post('/blogs', (req, res) => {
     const blog = new Blog(req.body)
 
     blog.save()
-        .then((result)=>{
+        .then((result) => {
             res.redirect('/blogs')
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log(err);
         })
 })
 
-app.get('/blogs/:id', (req, res)=>{
+app.get('/blogs/:id', (req, res) => {
     const id = req.params.id
     Blog.findById(id)
-        .then((result)=>{
+        .then((result) => {
             res.render('details', { blog: result, title: 'Blog Details' })
-        }).catch((err)=>{
-            console.log(err);
         })
 })
 
-app.get('/blogs/create', (req, res)=>{
-    res.render('create', { title:'Create a new Blog'})
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id
+
+    Blog.findByIdAndDelete(id)
+        .then((result) => {
+            res.json({ code: 200, message: 'Updated successfully', redirect: '/blogs' })
+        })
+        .catch((err) => { console.log(err); })
 })
 
 //redirect page
