0952cd9407fdc1459683cfb982d10640c30e200e
Omed0 <omedakram54@gmail.com>
2023-05-12 22:00:21 +0300

    sync

--- client/src/pages/users/user.jsx
+++ client/src/pages/users/user.jsx
@@ -1,20 +1,33 @@
-// import { Link } from 'react-router-dom'
-
 import { Link, Outlet } from "react-router-dom";
+import { useState } from 'react';
 
 
-export default function user(user) {
+export default function User() {
+    const [users, setUsers] = useState([{
+        _id: 1,
+        email: 'omed@gmail.com',
+        username: 'omed',
+        isAdmin: true,
+        password: 'iboy'
+    },]);
+
     return (
         <div className="blogs content">
             <h2>All Users</h2>
             {
-                user ?
-                    <>
-                        <Link className="single" to={`/auth/${user._id}`}>
-                            <h3 className="title">{user.email}</h3>
-                        </Link>
-                        <p className="snippet">{user.username}</p>
-                    </>
+                users.lenght ?
+
+                    users.map((user) => {
+                        return (
+                            <div key={user._id}>
+                                <Link className="single" to={`/auth/${user._id}`}>
+                                    <h3 className="title">{user.email}</h3>
+                                </Link>
+                                <p className="snippet">{user.username}</p>
+                            </div>
+                        )
+                    })
+
                     :
                     <>
                         <p>There are no User to display...</p>
