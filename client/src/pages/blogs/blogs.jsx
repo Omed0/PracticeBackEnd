e58c5988b674b7b2bc5b5e57db77f104d938b4f5
Omed0 <omedakram54@gmail.com>
2023-06-10 01:20:18 +0300

    fix error save specific post in redux now its working and rest it when leave page

--- client/src/pages/blogs/blogs.jsx
+++ client/src/pages/blogs/blogs.jsx
@@ -2,13 +2,12 @@ import { useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts } from "../../features/post/postService";
-// import { posts, loading, error, success } from "../../features/post/postsSlice";
 
 
 export default function blogs() {
 
   const dispatch = useDispatch();
-  const { posts, loading, error, success } = useSelector(state => state.posts);
+  const { posts, error } = useSelector(state => state.posts);
 
   useEffect(() => {
 
@@ -20,19 +19,17 @@ export default function blogs() {
     <div className="blogs content">
       <h2>All Blogs</h2>
       {
-        loading ? (<div>Loading...</div>)
+        error ? (<div>Error: {error.message}</div>)
           :
-          error ? (<div>Error: {error.message}</div>)
-            :
-            (success && posts && posts.length > 0 && posts.map((post, index) => (
-              <Link to={`/blogs/${post._id}`} key={index}>
-                <div className="blog-preview">
-                  <h2>{post.title}</h2>
-                  <p>Written by {post.author}</p>
-                </div>
-                <small>{post.snippet}</small>
-              </Link>
-            )))
+          (posts && posts.length > 0 && posts.map((post, index) => (
+            <Link to={`/blogs/${post._id}`} key={index}>
+              <div className="blog-preview">
+                <h2>{post.title}</h2>
+                <p>Written by {post.author}</p>
+              </div>
+              <small>{post.snippet}</small>
+            </Link>
+          )))
       }
       <Outlet />
     </div >
